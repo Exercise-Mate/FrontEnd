571c7fd0c141bc4241e3134058712178f2600b94
ingyeom <wjd1836@gmail.com>
2025-04-15 16:13:42 +0900

    Fix: merge 시 삭제된 JoinedAppointmentsPage 복원

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import AppointmentRoute from "@/router/Appointment";
 import MyClubsPage from "@/pages/MyClubsPage.vue";
 import MyAppointmentsPage from "@/pages/promise/MyAppointmentsPage.vue";
 import JoinedClubsPage from "@/pages/club/JoinedClubsPage.vue";
+import JoinedAppointmentsPage from "@/pages/promise/JoinedAppointmentsPage.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -21,6 +22,7 @@ const router = createRouter({
     },
     // (임시) 아래 두 줄을 다른 라우터 파일에 분리?
     { path: "/joinedClubs", name: "JoinedClubs", component: JoinedClubsPage },
+    { path: "/joinedAppointments", name: "JoinedAppointments", component: JoinedAppointmentsPage },
 
     ...AppointmentRoute,
     // { path: '/input', name: "Input", component: Input },
